36cf55dfdd0ebd4418d59740e8863d47eb6163b5
sedx876 <swatkins876@gmail.com>
2021-01-11 10:01:01 -0500

    initial setup of bootcamp routes

---
+++ routes/bootcamps.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) =>{
+  res
+  .status(200)
+  .json({ success: true, msg: 'Show all Bootcamps' })
+})
+
+router.get('/:id', (req, res) =>{
+  res
+  .status(200)
+  .json({ success: true, msg: 'Show bootcamp ${req.params.id}' })
+})
+
+router.post('/', (req, res) =>{
+  res
+  .status(200)
+  .json({ success: true, msg: 'Create Bootcamp' })
+})
+
+router.put('/:id', (req, res) =>{
+  res
+  .status(200)
+  .json({ success: true, msg: 'Update Bootcamp ${req.params.id}' })
+})
+
+router.delete('/:id', (req, res) =>{
+  res
+  .status(200)
+  .json({ success: true, msg: 'Delete Bootcamp ${req.params.id}' })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express')
 const dotenv = require('dotenv')
 const colors = require('colors')
 
+//Route Files
+const bootcamps = require('./routes/bootcamps')
+
 //Load env Variables
 dotenv.config({ path: './config/config.env'})
 
 const app = express()
+
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
