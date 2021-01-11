06b8cffbd20b95927009383a47969c15ae8a9970
sedx876 <swatkins876@gmail.com>
2021-01-11 13:36:42 -0500

    added config/db.js

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const colors = require('colors')
+const connectDB = require('./config/db')
+
 
 //Middleware
 const logger = require('./middleware/logger')
 
-//Route Files
-const bootcamps = require('./routes/bootcamps')
-
 //Load env Variables
 dotenv.config({ path: './config/config.env'})
+
+//Connect to DB
+connectDB()
+
+//Route Files
+const bootcamps = require('./routes/bootcamps')
 
 const app = express()
 
@@ -26,6 +31,13 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, 
+const server = app.listen(PORT, 
   console.log(`Server running in ${process.env.NODE_ENV} mode on Port ${process.env.PORT}`.rainbow.bold.underline)
 )
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error ${err.message}`)
+  //Close server & exit process
+  server.close(() => process.exit(1))
+})
