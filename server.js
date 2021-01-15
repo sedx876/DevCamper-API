6518b9791f3f6518abbe4cc0e5dfdc81676f5400
sedx876 <swatkins876@gmail.com>
2021-01-15 08:10:08 -0500

    Added auth and user routes models and controllers

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ connectDB()
 //Route Files
 const bootcamps = require('./routes/bootcamps')
 const courses = require('./routes/courses')
+const auth = require('./routes/auth')
+const users = require('./routes/users')
 
 const app = express()
 
@@ -38,6 +40,8 @@ app.use(fileupload())
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
 app.use('/api/v1/courses', courses)
+app.use('/api/v1/auth', auth)
+app.use('/api/v1/users', users)
 
 //Error Handler
 app.use(errorHandler)
