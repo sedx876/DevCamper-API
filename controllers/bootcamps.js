d4868646c877731d69e39a593fce18af5c207693
sedx876 <swatkins876@gmail.com>
2021-01-16 07:57:39 -0500

    code update

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -26,37 +26,85 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 //route POST /api/v1/bootcamps
 //access Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.create(req.body)
-    res.status(201).json({
+  // Add user to req,body
+  req.body.user = req.user.id;
+
+  // Check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  // If the user is not an admin, they can only add one bootcamp
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `The user with ID ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
+
+  const bootcamp = await Bootcamp.create(req.body);
+
+  res.status(201).json({
     success: true,
     data: bootcamp
-  })
-})
+  });
+});
 
 //Update Bootcamp
 //route PUT /api/v1/bootcamps/:id
 //access Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true 
-    })
-    if(!bootcamp){
-      return next(new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`, 404))
-    }
-    res.status(200).json({ success: true, data: bootcamp })
+  let bootcamp = await Bootcamp.findById(req.params.id)
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    )
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update this bootcamp`,
+        401
+      )
+    )
+  }
+
+  bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  })
+
+  res.status(200).json({ success: true, data: bootcamp })
 })
 
 //Delete Bootcamp
 //route DELETE /api/v1/bootcamps/:id
 //access Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
-    if(!bootcamp){
-      return next(new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`, 404))
-    }
-    bootcamp.remove()
-    res.status(200).json({ success: true, count: bootcamps.length, data: {} })
+  const bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to delete this bootcamp`,
+        401
+      )
+    );
+  }
+
+  await bootcamp.remove();
+
+  res.status(200).json({ success: true, data: {} });
 })
 
 // @desc      Get bootcamps within a radius
@@ -98,15 +146,15 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
     )
   }
 
-  // // Make sure user is bootcamp owner
-  // if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
-  //   return next(
-  //     new ErrorResponse(
-  //       `User ${req.user.id} is not authorized to update this bootcamp`,
-  //       401
-  //     )
-  //   );
-  // }
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update this bootcamp`,
+        401
+      )
+    );
+  }
 
   if (!req.files) {
     return next(new ErrorResponse(`Please upload a file`, 400));
--- routes/courses.js
+++ routes/courses.js
@@ -23,13 +23,13 @@ router
   }),
   getCourses
 )
-.post(protect, addCourse)
+.post(protect, authorize('publisher', 'admin'), addCourse)
 
 router
 .route('/:id')
 .get(getCourse)
-.put(protect, updateCourse)
-.delete(protect, deleteCourse)
+.put(protect, authorize('publisher', 'admin'), updateCourse)
+.delete(protect, authorize('publisher', 'admin'), deleteCourse)
 
 
 module.exports = router
