3eee74dfbeada4d43b0bfa6a6d9c4d3e23f0d6d0
sedx876 <swatkins876@gmail.com>
2021-01-17 08:24:04 -0500

    update code

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,31 @@
-const express = require('express');
+const express = require('express')
 const {
   getUsers,
   getUser,
   createUser,
   updateUser,
   deleteUser
-} = require('../controllers/users');
+} = require('../controllers/users')
 
-const User = require('../models/User');
+const User = require('../models/User')
 
-const router = express.Router({ mergeParams: true });
+const router = express.Router({ mergeParams: true })
 
-const advancedResults = require('../middleware/advancedResults');
-const { protect, authorize } = require('../middleware/auth');
+const advancedResults = require('../middleware/advancedResults')
+const { protect, authorize } = require('../middleware/auth')
 
-router.use(protect);
-router.use(authorize('admin'));
+router.use(protect)
+router.use(authorize('admin'))
 
 router
   .route('/')
   .get(advancedResults(User), getUsers)
-  .post(createUser);
+  .post(createUser)
 
 router
   .route('/:id')
   .get(getUser)
   .put(updateUser)
-  .delete(deleteUser);
+  .delete(deleteUser)
 
 module.exports = router
